d0b1682e1730a8cffff73749b3e7ab790f632be1
gochild-cracker <vmwendwa1@outlook.com>
2026-02-20 16:02:47 +0300

    Refactor VUCourses class structure and formatting

--- VUCourses.java
+++ VUCourses.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package vic.vupay;
 
-/**
- *
- * @author Heifer
- */
 public class VUCourses {
 
     
@@ -58,4 +51,5 @@ public class VUCourses {
     }
 }
     
+
 
